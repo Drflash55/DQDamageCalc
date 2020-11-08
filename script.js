d762c75faa060d79eb96e93ef319a3015d326673
DQDamageCalc <74132718+DQDamageCalc@users.noreply.github.com>
2020-11-08 16:31:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-$(document).ready(function(){
+$(document).ready(function() {
   $("p").hide()
+});
+
+function calculate(wep, arm, helm, skill) {
+ return Math.floor((wep * (0.6597 + 0.013202 * skill)*(arm+helm)*0.0028)*82);
+}
+
+$("#calc").click(function() {
+  let wep = $("#wep").val();
+  let arm = $("#arm").val();
+  let helm = $("hel").val();
+  let skill = $("#skill").val()
+  
+  const BaseDamage = calculate(wep, arm, helm, skill);
+  
+  alarm(BaseDamage);
+  
 });
