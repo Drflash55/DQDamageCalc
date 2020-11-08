29f21dd9960ed90442162f1581494cc3e0ef020a
DQDamageCalc <74132718+DQDamageCalc@users.noreply.github.com>
2020-11-08 17:13:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ function calculate(wep, arm, helm, skill) {
  return Math.floor((wep * (0.6597 + 0.013202 * skill)*((arm+helm)*0.0028))*82);
 }
 
+const Paragraph = $("p")
+const BaseText = Paragraph.text();
 function OnClick() {
   console.log("Clicc!");
   let wep = $("#wep").val();
@@ -17,5 +19,34 @@ function OnClick() {
   
   console.log(wep,arm,helm,skill)
   console.log(BaseDamage);
+  
+  let DamageArray = [];
+  
+  //No Inner
+  DamageArray.push(BaseDamage*0.95);
+  DamageArray.push(BaseDamage*1);
+  DamageArray.push(BaseDamage*1.05);
+  
+  //With Inner
+  DamageArray.push(BaseDamage*1.8*0.95);
+  DamageArray.push(BaseDamage*1.8*1);
+  DamageArray.push(BaseDamage*1.8*1.05);
+  
+  //With Enhanced Inner
+  DamageArray.push(BaseDamage*1.9*0.95);
+  DamageArray.push(BaseDamage*1.9*1);
+  DamageArray.push(BaseDamage*1.9*1.05);
+  
+  const regExp = /\((\d)\)/g;
+  let text = BaseText;
+  const matches = text.matchAll(regExp);
+  
+  for (const match of matches) {
+    console.log(match[0]);
+    text.replace(`(${match[0]})`, DamageArray[parseInt(match[0])]);
+  };
+  
+  Paragraph.text(text);
+  Paragraph.fadeIn();
   
 };
