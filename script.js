cb86f7b6ae656a3ddd5a5c48351526ab097257a5
DQDamageCalc <74132718+DQDamageCalc@users.noreply.github.com>
2020-11-08 18:04:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $(function() {
 
   console.log($("p").text())
   console.log(BaseText)
-  $("button").click(function() {
+  $("form button").click(function() {
     console.log("Clicc!");
     let wep = $("#wep").val();
     let arm = $("#arm").val();
