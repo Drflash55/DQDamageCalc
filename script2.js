47b6c3ab3f96595394532f003626518c2c6952c6
DQDamageCalc <74132718+DQDamageCalc@users.noreply.github.com>
2021-01-16 19:43:58 +0700

    Update script2.js

--- script2.js
+++ script2.js
@@ -3,7 +3,7 @@ $(function() {
   const BaseText = $("p").html();
   
   function calculate(wep, arm, helm, skill) {
-    return Math.floor((wep * (0.6597 + 0.013202 * skill)*((arm+helm)*0.0028))*91.1);
+    return Math.floor((wep * (0.6597 + 0.013202 * skill)*((arm+helm)*0.0028))*91.15);
   }
   
   function commarize(num) {
